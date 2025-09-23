900565168dafcb77801c9ddcc6b4faee1f765d9a
Clyde Limbaga <clydes0218@gmail.com>
2025-09-23 10:29:10 +0800

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,35 +2,68 @@ import java.util.Scanner;
 public class BubbleSort {
     public static void main( String[] args ) {
 
-        Scanner input = new Scanner ( System.in );
+        Scanner input = new Scanner(System.in);
         int temp;
-        System.out.print("Enter size of array >> ");
+        boolean run = true;
+
+        System.out.print("Enter size of the array >> ");
         int size = input.nextInt();
-        int[] array = new int[size];
+        int[] arr = new int[size];
 
         for ( int i = 0; i < size; i++ ) {
-            System.out.println("Enter element " + ( i + 1) + " ");
-            array[i] = input.nextInt();
-        }
-        System.out.print("Unsorted array >> ");
-        for ( int i = 0; i < array.length; i++ ) {
-            System.out.print(array[i] + " ");
+            System.out.println("Enter element #" + ( i + 1 ));
+            arr[i] = input.nextInt();
         }
 
-        for ( int i = 0; i < size - 1; i++ ) {
-            for ( int j = 0; j < size - 1; j++ ) {
-                if ( array[j] > array[j + 1] ) {
-                    temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                }
+        System.out.print("Elements inputted >> ");
+        for ( int i = 0; i < size; i++ ) {
+            System.out.print( arr[i] + " ");
+        }
+
+            System.out.println(" ");
+            System.out.println("Choose the Order");
+            System.out.println("1. Ascending Order");
+            System.out.println("2. Descending Order");
+            System.out.println("3. Exit");
+            int choice = input.nextInt();
+            for (int i = 0; i < size; i++) {
+                System.out.print(arr[i] + " ");
             }
-        }
-        System.out.println();
-        System.out.print("Sorted array >> ");
-        for ( int i = 0; i < size; i++ ) {
-            System.out.print(array[i] + " ");
-        }
+            System.out.println(" ");
+
+            if (choice == 1) {
+                for (int i = 0; i < size - 1; i++) {
+                    for (int j = 0; j < size - 1; j++) {
+                        if (arr[j] > arr[j + 1]) {
+                            temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+                        }
+                    }
+                    for (int k = 0; k < size; k++) {
+                        System.out.print(arr[k] + " ");
+                    }
+                    System.out.println(" ");
+                }
+            } else if (choice == 2) {
+                for (int i = 0; i < size - 1; i++) {
+                    for (int j = 0; j < size - 1; j++) {
+                        if (arr[j] < arr[j + 1]) {
+                            temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+                        }
+                    }
+                    for (int k = 0; k < size; k++) {
+                        System.out.print(arr[k] + " ");
+                    }
+                    System.out.println(" ");
+                }
+            } else if ( choice == 3 ) {
+                run = false;
+            } else {
+                System.out.println("Invalid Choice");
+            }
 
     }
 }
